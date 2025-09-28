bd2c9bfa8c4e5c10a7cff938115519d665ad66b2
SystemFalse <gud4enkoa@yandex.ru>
2025-09-28 18:39:57 +0300

    Initialized base Spring project

---
+++ src/main/java/io/github/systemfalse/wolfbot/WolfBotApplication.java
@@ -0,0 +1,13 @@
+package io.github.systemfalse.wolfbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WolfBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/systemfalse/wolfbot/WolfBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.systemfalse.wolfbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
